13c7d249faf9ffdd0bd1f049c9957155cb8ebd34
Bruno Carvalho <bruno.carvalho310303@gmail.com>
2024-03-28 06:17:37 -0300

    Configura variáveis de ambiente

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,10 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  ENV['API_KEY'] = "SUA CHAVE GOOGLE TRANSLATE API"
+  ENV['BASE_URL'] = "https://translation.googleapis.com/language/translate/v2/"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
