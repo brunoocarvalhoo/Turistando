36e84ea4b7f78a765427e26c5842e42633eb35a1
Bruno Carvalho <bruno.carvalho310303@gmail.com>
2024-03-28 06:15:10 -0300

    Implementa busca por tipo e tradução por parametro

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,62 +1,82 @@
-module API
-  module V1
-    class PlacesController < ApplicationController
-      before_action :set_place, only: %i[ show update destroy ]
+class PlacesController < ApplicationController
+  before_action :target
+  before_action :set_place, only: %i[ show update destroy ]
 
-      # GET /places
-      def index
-        @places = Place.all
+  # GET /places
+  def index
+    @places = Place.all
 
-        render json: @places
-      end
+    render json: @places
+  end
 
-      # GET /places/1
-      def show
-        render json: @place
-      end
+  # GET /places/1
+  def show
+    render json: @place
+  end
 
-      # POST /places
-      def create
-        @place = Place.new(place_params)
+  # POST /places
+  def create
+    @place = Place.new(place_params)
 
-        if @place.save
-          render json: @place, status: :created, location: @place
-        else
-          render json: @place.errors, status: :unprocessable_entity
-        end
-      end
-
-      # PATCH/PUT /places/1
-      def update
-        if @place.update(place_params)
-          render json: @place
-        else
-          render json: @place.errors, status: :unprocessable_entity
-        end
-      end
-
-      # DELETE /places/1
-      def destroy
-        @place.destroy
-      end
-
-      def specifcs_kinds
-        kind = params[:kind]
-        @places = Place.where("kinds LIKE ?", "%#{kind}%")
-
-        render json: @places
-      end
-
-      private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_place
-        @place = Place.find(params[:id])
-      end
-
-      # Only allow a list of trusted parameters through.
-      def place_params
-        params.require(:place).permit(:xid, :name, :rate, :osm, :wikidata, :kinds, :coordinates)
-      end
+    if @place.save
+      render json: @place, status: :created, location: @place
+    else
+      render json: @place.errors, status: :unprocessable_entity
     end
+  end
+
+  # PATCH/PUT /places/1
+  def update
+    if @place.update(place_params)
+      render json: @place
+    else
+      render json: @place.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /places/1
+  def destroy
+    @place.destroy
+  end
+
+  def target
+    @target = params[:target] ? params[:target] : 'en'
+  end
+
+  def specific_kinds
+    kind = params[:kind]
+    places = Place.where("kinds LIKE ?", "%#{kind}%").pluck(:name, :kinds)
+    places_json = places.to_json
+
+    translated_text(places_json)
+
+    formated_places = formated_text(@request['data']['translations'][0]['translatedText'])
+
+    render json: formated_places.as_json
+  end
+
+  def translated_text(text)
+    @request = HTTParty.post("#{ENV['BASE_URL']}?key=#{ENV['API_KEY']}", {
+      body: {
+        q: text,
+        target: @target
+      }
+    })
+  end
+
+  def formated_text(text)
+    text.gsub(/&quot;/, "\"")
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_place
+    @place = Place.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def place_params
+    params.require(:place).permit(:xid, :name, :rate, :osm, :wikidata, :kinds, :coordinates)
   end
 end
